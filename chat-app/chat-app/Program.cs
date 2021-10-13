59ead707047044255a3096425dc1a2eaff047e14
yami lahat <yamilahat@gmail.com>
2021-10-13 23:11:46 +0300

    fixed main

--- chat-app/chat-app/Program.cs
+++ chat-app/chat-app/Program.cs
@@ -6,13 +6,20 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            // Server.Start();
             try
             { 
                 DatabaseManager.InitDatabase();
             } catch (Exception e)
             {
                 Console.WriteLine($"There was an error initializing the database: {e}");
+            }
+
+            try
+            {
+                Server.Start();
+            } catch (Exception e)
+            {
+                Console.WriteLine($"Server error: {e}");
             }
         }
     }
